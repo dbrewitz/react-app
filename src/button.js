6a37fc07bcc1101947335e8fae52fe3151746d02
dbrewitz <Dtbrewitz@Brewitz-MacBook-Air.local>
2016-10-19 21:01:13 -0500

    commit

---
+++ src/button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+var MyButton = React.createClass({
+    render: function () {
+        return ( 
+            <button > My Button </button>
+        )
+    }
+})
+module.exports = MyButton
