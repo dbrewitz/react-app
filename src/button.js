1daa532ce07b1bd78e52cea06b79b77e8b9db856
dbrewitz <Dtbrewitz@Brewitz-MacBook-Air.local>
2016-10-19 21:34:15 -0500

    class on button

--- src/button.js
+++ src/button.js
@@ -1,8 +1,15 @@
 import React from 'react';
+
 var MyButton = React.createClass({
+    handleClick: function(e){
+       // alert('hi')
+       console.log(e)
+     //  e.className.add('yo')
+    },
     render: function () {
+            var myClass = "myButton"
         return ( 
-            <button > My Button </button>
+            <button className={myClass} onClick={this.handleClick}> My Button </button>
         )
     }
 })
